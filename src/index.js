661f8746f5bc29a587091a6d55859a974ddef824
elgnis3900 <jonathanduarte313@gmail.com>
2020-12-20 21:04:34 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from "react-dom";
+import { ApolloProvider } from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import './index.css';
-import App from './App';
+import Planets from "./components/Planets";
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const url = "https://liberal-alien-90.hasura.app/v1/graphql"
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: url,
+  }),
+});
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <Planets />
+  </ApolloProvider>
 );
+
+render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
